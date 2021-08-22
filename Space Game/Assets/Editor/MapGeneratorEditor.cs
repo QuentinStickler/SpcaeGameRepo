5c9efde68d107cb9fe0bf1c7abbaf270f5c34ce2
QuentinStickler <quentin.stickler@yahoo.com>
2021-08-22 12:16:22 +0200

    New scene for terrainbuilding, First NoiseMap

---
+++ Space Game/Assets/Editor/MapGeneratorEditor.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(MapGenerator))]
+public class MapGeneratorEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        MapGenerator mapGenerator = (MapGenerator) target;
+        
+        if(DrawDefaultInspector()){
+            if (mapGenerator.autoUpdate)
+            {
+                mapGenerator.generateMap();
+            }}
+
+        
+        if(GUILayout.Button("Generate Map")){mapGenerator.generateMap();}
+    }
+}
